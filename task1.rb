be1f8c8eaf6625173f52a46c70425081c859ad35
NoHomey <ivo_stratev@abv.bg>
2014-10-15 22:19:22 +0300

    Create task1.rb

---
+++ task1.rb
@@ -0,0 +1,27 @@
+require 'csv'
+
+a = Hash.new
+Dir.glob("#{ARGV[0]}/*.rb") do |dir_file_name|
+
+	file_name = dir_file_name.split(/\//).last.to_s
+	
+	digit = file_name.split(/\_/).last.split(/\./).first.to_s
+	
+	first_name = file_name.split(/_/).first.to_s
+	full_first_name = first_name + digit
+	full_first_name = full_first_name.to_s
+	
+	tmp = file_name.split("#{first_name}_")
+	full_last_name = tmp.last.split(/_/).first.to_s + digit
+	full_last_name = full_last_name.to_s
+	
+	condition = full_last_name.length - digit.length
+	if (condition == 7)
+		a[full_first_name.to_s] = full_last_name.to_s
+	end	
+end
+
+CSV.open("results.csv", "w") do |csv|
+	a.values.sort
+	a.each {|key, value| csv << [key,' ' + value]}
+end
