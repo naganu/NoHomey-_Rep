60f9395fb119d77082208528c9c1b0b5a7ac8303
NoHomey <ivo_stratev@abv.bg>
2014-10-16 08:39:37 +0300

    Update task1.rb

--- task1.rb
+++ task1.rb
@@ -1,11 +1,11 @@
 require 'csv'
-
-a = Hash.new
+a = Array.new
+h = Hash.new
 Dir.glob("#{ARGV[0]}/*.rb") do |dir_file_name|
 
 	file_name = dir_file_name.split(/\//).last.to_s
 	
-	digit = file_name.split(/\_/).last.split(/\./).first.to_s
+	digit = file_name.split(/_/).last.split(/\./).first.to_s
 	
 	first_name = file_name.split(/_/).first.to_s
 	full_first_name = first_name + digit
@@ -17,11 +17,14 @@ Dir.glob("#{ARGV[0]}/*.rb") do |dir_file_name|
 	
 	condition = full_last_name.length - digit.length
 	if (condition == 7)
-		a[full_first_name.to_s] = full_last_name.to_s
+		h[full_first_name.to_s] = full_last_name.to_s
 	end	
 end
 
 CSV.open("results.csv", "w") do |csv|
-	a.values.sort
-	a.each {|key, value| csv << [key,' ' + value]}
+	a = h.sort
+	a.each do |element|
+		csv << element
+	end
 end
+
