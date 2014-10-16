390ab8d8b8b1ffa425956d6ffa16ba660cab9021
NoHomey <ivo_stratev@abv.bg>
2014-10-16 08:38:25 +0300

    Update task3.rb

--- task3.rb
+++ task3.rb
@@ -1,17 +1,29 @@
 require 'csv'
-a = Hash.new
-Dir.glob(ARGV.shift+"/*.rb" ) do |dir_file_name|
-	puts "#{file_name}"
-	file_name = dir_file_name.split(/\//).last.to_s
-	l = file_name.length.to_s
-	if(/\w*(_)\w*(_)\d*(.rb)/.match(file_name))
-	else
-		file_length = file_name.length.to_s
-		a[file_length] = file_name
+h = Hash.new
+a = Array.new
+Dir.glob("#{ARGV[0]}/*.rb") do |dir_file_name_1|
+	Dir.glob("#{ARGV[1]}/*.rb") do |dir_file_name_2|
+	
+		file_name_1 = dir_file_name_1.split(/\//).last.to_s
+		file_name_2 = dir_file_name_2.split(/\//).last.to_s
+	
+		if(/[A-Za-z]+(_)[A-Za-z]+(_)\d+(.rb)/.match(file_name_1))
+		else
+			file_length = file_name_1.length.to_s
+			h[file_name_1] = file_length
+		end
+	
+		if(/[A-Za-z]+(_)[A-Za-z]+(_)\d+(.rb)/.match(file_name_2))
+		else
+			file_length = file_name_2.length.to_s
+			h[file_name_2] = file_length
+		end
 	end
 end
 
 CSV.open("results.csv", "w") do |csv|
-	a.values.sort
-	a.each {|key, value| csv << [value,' ' + key]}
+	a = h.sort
+	a.each do |element|
+		csv << element
+	end
 end
