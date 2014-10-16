111627ef1a02de452da4cd93caf5d155c09b2ba8
NoHomey <ivo_stratev@abv.bg>
2014-10-16 08:38:48 +0300

    Update task4.rb

--- task4.rb
+++ task4.rb
@@ -1,17 +1,21 @@
 require 'csv'
-
-a = Hash.new
+a = Array.new
+h = Hash.new
 Dir.glob("#{ARGV[0]}/*.rb" ) do |dir_file_name|
 	file_name = dir_file_name.split(/\//).last.to_s
 	file_name = file_name.split(/\./).first.to_s
 	l = file_name.length.to_s
 	if(l <=> ARGV[1]) != 0
 		file_length = file_name.length.to_s
-		a[file_length] = file_name
+		h[file_length] = file_name
 	end
 end
 
 CSV.open("results.csv", "w") do |csv|
-	a.values.sort.reverse
-	a.each {|key, value| csv << [value,' ' + key]}
+	
+	a = h.sort
+
+	a.each do |element|
+		csv << element
+	end
 end
