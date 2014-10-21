b4dc519690bf5f9bb214bed8f6b49bcb7c5ba76c
NoHomey <ivo_stratev@abv.bg>
2014-10-21 18:36:06 +0300

    Update task_1R_2.rb

--- task_1R_2.rb
+++ task_1R_2.rb
@@ -16,15 +16,12 @@ require 'csv'
 a = Array.new
 h = Hash.new
 Dir.glob("#{ARGV[0]}/*.rb") do |dir_file_name|
-
+	
 	file_name = dir_file_name.split(/\//).last.to_s
-	
 	digit = file_name.split(/_/).last.split(/\./).first.to_s
-	
 	first_name = file_name.split(/_/).first.to_s
 	full_first_name = first_name + digit
 	full_first_name = full_first_name.to_s
-	
 	tmp = file_name.split("#{first_name}_")
 	full_last_name = tmp.last.split(/_/).first.to_s + digit
 	full_last_name = full_last_name.to_s
